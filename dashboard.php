505e018628cf7c4f10cb2b8335d9fc6eba336000
Valeria Monroy Dominguez <ingvalmd15@gmail.com>
2026-04-30 14:27:44 -0600

    correcto

--- dashboard.php
+++ dashboard.php
@@ -2,7 +2,7 @@
 session_start();
 
 // ¿Existe la sesión? Si no, fuera de aquí.
-if (!isset($_SESSION['id'])) {
+if (!isset($_SESSION['id_usuario'])) {
     header("Location: index.html");
     exit();
 }
